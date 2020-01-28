99e8d599dca3e2bff03a0d060e31963bf8ec923d
Gabriel Medeiros Abrahão <30908904+gabriel-abrahao@users.noreply.github.com>
2020-01-28 14:51:06 -0800

    Update nc_to_df.R

--- nc_to_df.R
+++ nc_to_df.R
@@ -1,3 +1,5 @@
+# Just extract the non-NaN points from the computed rasters as a single dataframe per crop
+
 library(tidyverse)
 library(raster)
 
@@ -8,49 +10,51 @@ crops = c("Soybeans")
 years = 2000:2002
 
 
-crop = crops[1]
-
-allydata = data.frame()
-for (year in years) {
-  infname = paste0(infolder,crop,".computed.",year,".nc")
+for (crop in crops) {
   
-  tempmean = raster(infname, varname = "tempmean")
-  tmaxmean = raster(infname, varname = "tmaxmean")
-  tminmean = raster(infname, varname = "tminmean")
-  precmean = raster(infname, varname = "precmean")
+  allydata = data.frame()
+  for (year in years) {
+    infname = paste0(infolder,crop,".computed.",year,".nc")
+    
+    tempmean = raster(infname, varname = "tempmean")
+    tmaxmean = raster(infname, varname = "tmaxmean")
+    tminmean = raster(infname, varname = "tminmean")
+    precmean = raster(infname, varname = "precmean")
+    
+    tempdist = brick(infname, varname = "tempdist")
+    tempgdds = brick(infname, varname = "tempgdds")
+    tempdistzvals = getZ(tempdist)
+    tempgddszvals = getZ(tempgdds)
+    
+    tempdist = as.data.frame(tempdist, xy = TRUE)
+    colnames(tempdist)[-(1:2)] <- gsub("-","m",paste0("tdi",tempdistzvals))
+    
+    tempgdds = as.data.frame(tempgdds, xy = TRUE)
+    colnames(tempgdds)[-(1:2)] <- gsub("-","m",paste0("gdd",tempgddszvals))
+    
+    tempmean = as.data.frame(tempmean, xy = TRUE)
+    tmaxmean = as.data.frame(tmaxmean, xy = TRUE) 
+    tminmean = as.data.frame(tminmean, xy = TRUE) 
+    precmean = as.data.frame(precmean, xy = TRUE)
+    
+    ydata <- tempmean %>% 
+      mutate(pid = group_indices(.,x,y)) %>%
+      left_join(tmaxmean, by = c("x","y")) %>% 
+      left_join(tminmean, by = c("x","y")) %>% 
+      left_join(precmean, by = c("x","y")) %>%
+      left_join(tempdist, by = c("x","y")) %>% 
+      left_join(tempgdds, by = c("x","y")) %>%
+      na.omit()
+    ydata$year = year
+    
+    # Reorder the columns to get the most informative ones first
+    firstvars = c("pid","x","y","year")
+    varorder = c(firstvars,colnames(ydata)[!(colnames(ydata) %in% firstvars)])
+    ydata = ydata[,varorder]
+    
+    allydata = rbind(allydata,ydata)
+  }
   
-  tempdist = brick(infname, varname = "tempdist")
-  tempgdds = brick(infname, varname = "tempgdds")
-  tempdistzvals = getZ(tempdist)
-  tempgddszvals = getZ(tempgdds)
-  
-  tempdist = as.data.frame(tempdist, xy = TRUE)
-  colnames(tempdist)[-(1:2)] <- gsub("-","m",paste0("tdi",tempdistzvals))
-  
-  tempgdds = as.data.frame(tempgdds, xy = TRUE)
-  colnames(tempgdds)[-(1:2)] <- gsub("-","m",paste0("gdd",tempgddszvals))
-  
-  tempmean = as.data.frame(tempmean, xy = TRUE)
-  tmaxmean = as.data.frame(tmaxmean, xy = TRUE) 
-  tminmean = as.data.frame(tminmean, xy = TRUE) 
-  precmean = as.data.frame(precmean, xy = TRUE)
-  
-  ydata <- tempmean %>% 
-    mutate(pid = group_indices(.,x,y)) %>%
-    left_join(tmaxmean, by = c("x","y")) %>% 
-    left_join(tminmean, by = c("x","y")) %>% 
-    left_join(precmean, by = c("x","y")) %>%
-    left_join(tempdist, by = c("x","y")) %>% 
-    left_join(tempgdds, by = c("x","y")) %>%
-    na.omit()
-  ydata$year = year
-  
-  # Reorder the columns to get the most informative ones first
-  firstvars = c("pid","x","y","year")
-  varorder = c(firstvars,colnames(ydata)[!(colnames(ydata) %in% firstvars)])
-  ydata = ydata[,varorder]
-  
-  allydata = rbind(allydata,ydata)
+  outfname = paste0(outfolder,crop,".climdata.rds")
+  saveRDS(allydata,file = outfname)
 }
-
-
