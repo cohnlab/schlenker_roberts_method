d95c056487ad1eb880559eee4bcf61474516667e
Gabriel Abrahao <gabrielmabrahao@gmail.com>
2020-03-16 23:24:18 -0300

    Something is amiss

--- calibrate_sensitivity.R
+++ calibrate_sensitivity.R
@@ -14,7 +14,9 @@ names(gddsens) <- crops
 
 betasfolder = paste0("gdd_betas/Sacks_ZARC_fill_fill_120d/")
 
-caldata = data.frame(tmaxbase = c(27,27,30), deltat = c(1,6,1), deltay = c(-6.4,-55.9,-1))
+# caldata = data.frame(tmx = c(27,27,30), X.Temp = c(1,6,1), deltay = c(-6.4,-55.9,-1))
+caldata = read.csv("../moore_coefficients/includedstudies_revisions_tbaseline.csv")
+names(caldata)[names(caldata) == "Yield.change...."] <- "deltay"
 
 crop = "Maize"
 
@@ -22,14 +24,52 @@ crop = "Maize"
 betasedd = read.csv(paste0(betasfolder,crop,".betas.nEDD.csv"))
 betasgdd = read.csv(paste0(betasfolder,crop,".betas.nGDD.csv"))
 
-caldata$deltagdd = eval_nxdd(betasgdd,caldata$tmaxbase + caldata$deltat) - eval_nxdd(betasgdd,caldata$tmaxbase)
-caldata$deltaedd = eval_nxdd(betasedd,caldata$tmaxbase + caldata$deltat) - eval_nxdd(betasedd,caldata$tmaxbase)
+caldata$deltagdd = eval_nxdd(betasgdd,caldata$tmx + caldata$X.Temp) - eval_nxdd(betasgdd,caldata$tmx)
+caldata$deltaedd = eval_nxdd(betasedd,caldata$tmx + caldata$X.Temp) - eval_nxdd(betasedd,caldata$tmx)
 
 caldata$gddsens = gddsens[[crop]]
 
 caldata$deltayfrac = 0.01*caldata$deltay
 
 caldata$eddsens = (caldata$deltayfrac - caldata$gddsens*caldata$deltaedd)/caldata$deltaedd
+
+eddstudies = c("Schlenker and Lobell 2010","Schlenker and Roberts 2009")
+
+caldata$modeltype = "Mechanistic"
+caldata$modeltype[caldata$statmodel == 1] = "Statistical"
+caldata$modeltype[caldata$Reference %in% eddstudies] = "Statistical EDD"
+
+# Filter out positive impacts and specify the crop
+caldata %>% 
+  filter(Crop1 == crop) %>%
+  filter(eddsens <= 0) ->
+  data
+
+statdata = filter(data,modeltype != "Mechanistic")
+
+ggplot(filter(data,modeltype != "Mechanistic"),aes(x = tmx, y = eddsens)) +
+  geom_point(aes(color = modeltype),size = 3) + theme_classic()
+
+ggplot(statdata,aes(x = tmx, y = eddsens)) +
+  geom_point(aes(shape = modeltype, color = bootstrap.group),size = 4) + theme_classic()
+
+
+ggplot(filter(statdata, !(Reference %in% c("Moya et al", "Corobov"))),
+       aes(x = tmx, y = eddsens)) +
+  geom_point(aes(shape = modeltype, color = bootstrap.group),size = 4) + theme_classic()
+
+hlinevalue = -0.006435774
+ggplot(filter(statdata, (Reference %in% c("Schlenker and Roberts 2009"))),
+       aes(x = X.Temp, y = eddsens)) +
+  geom_point(aes(color = bootstrap.group),size = 4) + theme_classic() +
+  xlab("deltaT") + ylim(c(-0.02,-0.006)) + 
+  geom_hline(yintercept = hlinevalue) + 
+  geom_text(aes(2,hlinevalue,
+                label = paste("SR value = ",sprintf("%f", hlinevalue)), vjust = -1))
+
+# View(filter(statdata, (Reference %in% c("Schlenker and Roberts 2009"))))
+
+
 
 coef0 = 0.0
 coef1 = 1.0
