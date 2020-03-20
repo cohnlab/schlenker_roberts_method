f54fb15a192a69b1b223cc9c70ea9a19d369a516
Gabriel Abrahao <gabrielmabrahao@gmail.com>
2020-03-20 16:30:24 -0300

    Generates scenario tables based on the output of extract_both.R in globiom_clim_extract

---
+++ gen_scenario_tables.R
@@ -0,0 +1,46 @@
+require(tidyverse)
+
+infolder = "../globiom_clim_extract/output_tables/"
+outfolder = "scenario_tables/"
+
+invarnames = c("gccdtemp","gccdtmax")
+outvarnames = c("gccdtemp","gccdtemp")
+names(outvarnames) <- invarnames
+
+crops = c("Maize","Soybeans","Cotton")
+scenarios = c("ssp245","ssp585")
+
+# Create output folder
+dir.create(outfolder,showWarnings = F, recursive = T)
+
+alldata = data.frame()
+
+# crop = crops[1]
+# scenario = scenarios[1]
+# invarname = invarnames[1]
+for (crop in crops) {
+  for (scenario in scenarios) {
+    for (invarname in invarnames) {
+      outvarname = outvarnames[[invarname]]
+      
+      infname = paste0(infolder,crop,".",scenario,".",invarname,".all.csv")
+      indata = read.csv(infname)
+      indata %>% 
+        select(-c("XCOORD","YCOORD","RealArea_m")) %>% 
+        rename(ID = "COLROW30", Crop = "crop", scen = "rcp") %>%
+        gather(key = "ScenYear",value = "Value",-ID,-Crop,-scen) -> indata
+      indata$ScenYear = as.factor(as.numeric(substr(indata$ScenYear,2,5)))
+      indata$PARAMETER = outvarname
+      
+      alldata = rbind(alldata,indata)
+    }
+  }
+}
+# Put zero where NA
+alldata$Value[is.na(alldata$Value)] <- 0
+
+for (scenario in scenarios) {
+  outfname = paste0(outfolder,"deltaT",scenario,".csv")
+  alldata %>% filter(scen == scenario) -> outdata
+  write.csv(outdata, file = outfname, row.names = F)
+}
